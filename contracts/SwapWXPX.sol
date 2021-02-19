5d541d456833686701686f7b6d58d5db1f0bdddc
nqt123 <quythang1997@gmail.com>
2021-02-19 13:21:54 +0700

    Update custodian struct

--- contracts/SwapWXPX.sol
+++ contracts/SwapWXPX.sol
@@ -15,6 +15,24 @@ contract SwapWXPX is Ownable, AccessControl, Pausable, ReentrancyGuard {
 
     WXPX public token;
 
+    enum RequestStatus { PENDING, CANCELED, APPROVED, REJECTED }
+
+    struct Request {
+        address requester; // Owner of the request
+        uint amount; // Amount of token to mint/burn
+        string depositAddress; // User's XPX deposit address in burn, user's ETH address in mint
+        string txid; // Assets txid for sending / redemming asset in the burn / mint process
+        uint nonce; // Serial number allocated for each request
+        uint timestamp; // Time of the request creation.
+        RequestStatus status; // Status of the request.
+    }
+
+    // mapping between a mint request hash and the corresponding request nonce. 
+    mapping(bytes32 => uint) public mintRequestNonce;
+
+    // mapping between a burn request hash and the corresponding request nonce.
+    mapping(bytes32 => uint) public burnRequestNonce;
+
     constructor(WXPX _token) {
         require(address(_token) != address(0), "SwapWXPX::INVALID_TOKEN_ADDRESS");
 
@@ -29,7 +47,7 @@ contract SwapWXPX is Ownable, AccessControl, Pausable, ReentrancyGuard {
 
 
     // modifiers
-    
+
     /**
      * @notice Modifier to make a function callable only by custodian.
      */
@@ -38,7 +56,7 @@ contract SwapWXPX is Ownable, AccessControl, Pausable, ReentrancyGuard {
         _;
     }
 
-    // custodian functions
+    // Pause functions
 
     /**
      * @notice Pauses all request.
@@ -61,5 +79,30 @@ contract SwapWXPX is Ownable, AccessControl, Pausable, ReentrancyGuard {
     function unpause() public whenPaused virtual {
         require(hasRole(PAUSER_ROLE, _msgSender()), "WXPX: must have pauser role to unpause");
         _unpause();
+    }
+
+    function _calcRequestHash(Request memory request) internal pure returns (bytes32) {
+        return keccak256(abi.encode(
+            request.requester,
+            request.amount,
+            request.depositAddress,
+            request.txid,
+            request.nonce,
+            request.timestamp
+        ));
+    }
+
+    function getPendingMintRequest(bytes32 requestHash) internal view returns (uint nonce, Request memory request) {
+        require(requestHash != 0, "request hash is 0");
+        nonce = mintRequestNonce[requestHash];
+        request = mintRequests[nonce];
+        validatePendingRequest(request, requestHash);
+    }
+
+    function getPendingBurnRequest(bytes32 requestHash) internal view returns (uint nonce, Request memory request) {
+        require(requestHash != 0, "request hash is 0");
+        nonce = burnRequestNonce[requestHash];
+        request = burnRequests[nonce];
+        validatePendingRequest(request, requestHash);
     }
 }
