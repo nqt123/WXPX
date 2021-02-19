a90ca48e95107e003239e8b5e5c7daedb45acd84
nqt123 <quythang1997@gmail.com>
2021-02-19 11:53:03 +0700

    Update WXPX token

--- contracts/token/WXPX.sol
+++ contracts/token/WXPX.sol
@@ -10,8 +10,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN");
 
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
@@ -21,10 +22,12 @@ contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      */
     constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {
         _setupDecimals(decimals);
+
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
         _setupRole(PAUSER_ROLE, _msgSender());
-        _setupRole(CUSTODIAN, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(BURNER_ROLE, _msgSender());
     }
 
     /**
@@ -34,10 +37,10 @@ contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      *
      * Requirements:
      *
-     * - the caller must have the `CUSTODIAN`.
+     * - the caller must have the `MINTER_ROLE`.
      */
     function mint(address to, uint256 amount) public virtual {
-        require(hasRole(CUSTODIAN, _msgSender()), "ERC20PresetMinterPauser: must have custodian role to mint");
+        require(hasRole(MINTER_ROLE, _msgSender()), "WXPX: must have minter role to mint");
         _mint(to, amount);
     }
 
@@ -47,7 +50,7 @@ contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      * See {ERC20-_burn}.
      */
     function burn(uint256 amount) public override virtual {
-        require(hasRole(CUSTODIAN, _msgSender()), "ERC20PresetMinterPauser: must have custodian role to burn");
+        require(hasRole(BURNER_ROLE, _msgSender()), "WXPX: must have burner role to burn");
         super.burn(amount);
     }
 
@@ -61,7 +64,7 @@ contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      * - the caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to pause");
+        require(hasRole(PAUSER_ROLE, _msgSender()), "WXPX: must have pauser role to pause");
         _pause();
     }
 
@@ -75,7 +78,7 @@ contract WXPX is Context, AccessControl, ERC20Burnable, ERC20Pausable {
      * - the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to unpause");
+        require(hasRole(PAUSER_ROLE, _msgSender()), "WXPX: must have pauser role to unpause");
         _unpause();
     }
 
