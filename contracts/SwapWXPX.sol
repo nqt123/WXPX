020206bec387824f1301f3be7e1b2fe3ee3c9eb5
nqt123 <quythang1997@gmail.com>
2021-02-19 13:06:18 +0700

    Update custodian pause/unpause

--- contracts/SwapWXPX.sol
+++ contracts/SwapWXPX.sol
@@ -9,25 +9,57 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-
 contract SwapWXPX is Ownable, AccessControl, Pausable, ReentrancyGuard {
-  bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN");
+    bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-  WXPX public token;
+    WXPX public token;
 
-  constructor (WXPX _token) {
-    require(address(_token) != address(0), "SwapWXPX::INVALID_TOKEN_ADDRESS");
-    token = WXPX(_token);
+    constructor(WXPX _token) {
+        require(address(_token) != address(0), "SwapWXPX::INVALID_TOKEN_ADDRESS");
 
-    _setupRole(CUSTODIAN, msg.sender);
-  }
+        token = WXPX(_token);
 
-  modifier onlyCustodian() {
-    require(hasRole(CUSTODIAN, msg.sender), "SwapWXPX::NOT_AUTHORIZED");
-    _;
-  }
+        _setupRole(CUSTODIAN, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+    }
+    
 
-  // events
+    // events
 
-  
+
+    // modifiers
+    
+    /**
+     * @notice Modifier to make a function callable only by custodian.
+     */
+    modifier onlyCustodian() {
+        require(hasRole(CUSTODIAN, msg.sender), "SwapWXPX::NOT_AUTHORIZED");
+        _;
+    }
+
+    // custodian functions
+
+    /**
+     * @notice Pauses all request.
+     * Requirements:
+     * - contract is unpausing
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function pause() public whenNotPaused virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()),"WXPX: must have pauser role to pause");
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all request.
+     *
+     * Requirements:
+     * - contract is paused
+     * - the caller must have the `PAUSER_ROLE`.
+    */
+    function unpause() public whenPaused virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "WXPX: must have pauser role to unpause");
+        _unpause();
+    }
 }
